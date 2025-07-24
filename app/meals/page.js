1af88c17b8ab9be8417bec44c0c64891a357fd11
“Vishal” <vishalsoni2225@gmail.com>
2025-07-24 11:21:52 +0530

    added meals data and feteched the data from sqllite db

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,9 +1,29 @@
-export default function MealPage() {
+import Link from 'next/link';
+import classes from './page.module.css';
+import MealGrid from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
+
+export default async function MealPage() {
+  const meals = await getMeals();
   return (
-    <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
-        its meal page to get started!
+    <>
+    <header className={classes.header}>
+      <h1>Delicious meal, created{''}
+      <span className={classes.highlight}>by you!</span>
       </h1>
+      <p>Choose your favorite recipe and cook it yourself.  It is easy</p>
+      <p className={classes.cta}>
+        <Link href="/meals/share">
+          Share Your Favorite Recipe
+        </Link>
+        
+      </p>
+    </header>
+    <main className={classes.main}>
+      <MealGrid meals={meals} />
+        
     </main>
+    </>
+   
   );
 }
