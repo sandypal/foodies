fe341277ea30ab07539a1d1b1c44137e07a7efa9
Sandeep Kumar <vishalsoni2225@gmail.com>
2025-07-25 12:47:00 +0530

    created the meals detail page for vercel

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,14 +1,27 @@
-import { getMealBySlug } from '@/lib/meals';
+import { getMealBySlug, getMeals } from '@/lib/meals';
  import Image from 'next/image';
  
  import classes from './page.module.css';
 import { notFound } from 'next/navigation';
+
+
+export async function generateStaticParams() {
+  const meals = await getMeals();
+
+  return meals.map((meal) => ({
+    mealSlug: meal.slug,
+  }));
+}
+
 export default async function MealsDetailsPage({ params }) {
   const meals = await getMealBySlug(params.mealSlug);
   if (!meals) {
    notFound();
   }
-  meals.instructions = meals.instructions.replace(/(?:\r\n|\r|\n)/g, '<br>'); // Convert newlines to <br> for HTML rendering
+   // Convert line breaks to <br>
+  if (typeof meals.instructions === 'string') {
+    meals.instructions = meals.instructions.replace(/(?:\r\n|\r|\n)/g, '<br>');
+  }
   return (
     <>
     <header className={classes.header}>
